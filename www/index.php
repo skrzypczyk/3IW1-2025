c1f9a6def5edda001bd78a3495e4c544694f4864
Yves Skrzypczyk <ysk@wizards-technologies.com>
2025-09-22 14:11:46 +0200

    init docker

--- www/index.php
+++ www/index.php
@@ -33,12 +33,74 @@ $i++; // post incrémentation
 $i = $i + 1;
 $i += 1;
 
-
+/*
 $i=0;
-echo $i;//0
-echo $i++;//0
-echo $i;//1
-echo ++$i;//2
+$i++;
+echo $i++; //1
+echo --$i; //1
+echo $i + 1; //2
+echo $i += 1; //2
+echo $i; //2
+echo $i = $i + 1; //3
+echo $i; //3
+*/
+
+$myFirstName = "Yves";
+
+//echo "Bonjour ";
+//echo $myFirstName;
+
+// Concaténation
+//echo "Bonjour ".$myFirstName;
+
+// Inclusion mais à éviter trop source d'erreurs
+//echo "Bonjour $myFirstName";
+
+//Echaper les caractères
+//echo "Aujourd'hui on apprend le \"PHP\" ";
+
+
+//Conditions : IF
+/*
+if(condition){
+    //Instructions
+}else if(condition)
+    //une seule instruction
+    echo "Une seule instruction";
+else{
+    //Instruction
+}
+
+
+$age = 18;
+
+if($age == 18)
+    echo 'Tout juste majeur';
+elseif($age < 18){
+    echo 'Mineur';
+}else{
+    echo 'Majeur';
+}
+*/
+
+//Conditions : switch
+$role = "admin";
+switch ($role){
+    case "admin" :
+        echo "Tu as tous les droits d'administration";
+    case "editor" :
+        echo "Tu peux modifier tous les contenus";
+    case "author" :
+        echo "Tu peux modifier tes contenus";
+    default :
+        echo "Tu peux visualiser le site";
+        break;
+}
+
+
+
+
+
 
 
 
